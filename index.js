fdfd4f042facdca56b458d9febbe2773a8651642
Rakibul md <rakib245875@gmail.com>
2022-05-19 03:56:24 +0600

    added mongodb connection

--- index.js
+++ index.js
@@ -3,8 +3,27 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+//
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ju3nh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        await client.connect;
+        const tasksCollection = client.db("upkeep").collection("tasks");
+        console.log("db connected");
+    } finally {
+    }
+}
 
 app.get("/", (req, res) => {
     res.send("hello");
@@ -13,3 +32,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log("server running at port", port);
 });
+
+run().catch(console.dir);
